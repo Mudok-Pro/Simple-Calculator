c849259145a1cb6bc28c9a8a40fe73488b69916b
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-12 18:56:55 +0100

    created operate function , it uses three parameters (num1,num2,"operator") it return the proper result for each operation

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-function addThem(){
-    const digitA = document.getElementsByClassName("number"); 
-    const digitB = document.getElementsByClassName("number");
-    const result = digitA + digitB ; 
-    return result;
+function add(a, b) {
+    return a + b;
 }
 
-function subtractThem(){
-    const digitA = document.getElementsByClassName("number"); 
-    const digitB = document.getElementsByClassName("number");
-    const result = digitA - digitB ; 
-    return result;
+// Function to divide two numbers
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        throw new Error("Division by zero is not allowed");
+    }
 }
-function multiplyThem(){
-    const digitA = document.getElementsByClassName("number"); 
-    const digitB = document.getElementsByClassName("number");
-    const result = digitA * digitB ; 
-    return result;
+
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
 }
-function divideThem(){
-    const digitA = document.getElementsByClassName("number"); 
-    const digitB = document.getElementsByClassName("number");
-    const result = digitA / digitB ; 
-    if (digitB === 0){
-        alert("LMAO")
-    }else {
-    return result;
+
+// Function to subtract one number from another
+function subtract(a, b) {
+    return a - b;
 }
+let num1 ; 
+let num2 ; 
+let operator ; 
+ ////// a function to take two numbers and an operator 
+function operate(num1,num2,operator){
+    if (operator === "+"){
+        return add(num1,num2)
+    } else if (operator === "-"){
+        return subtract(num1,num2)
+    }else if (operator === "*"){
+        return multiply(num1,num2)
+    } else if (operator ==="/"){
+        return divide(num1,num2)
+    }
 }
+
+console.log(operate(2,5,"+"))
+console.log(operate(2,5,"-"))
+console.log(operate(2,5,"/"))
